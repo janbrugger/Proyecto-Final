9a8ce70cd62bff6f312e6c52812e2b8475c5c53f
Jan B <janbrugger00@gmail.com>
2023-08-30 15:30:52 -0300

    Buscador funcionando
    Falta emprolijar la escritura

--- workspace-inicial/js/products.js
+++ workspace-inicial/js/products.js
@@ -44,3 +44,50 @@ function showData(dataArray) {
   }
 }
 
+
+const searchInput = document.getElementById('buscador'); //toma datos del input
+const contenidoProductos = document.querySelector(".pb-5 .container"); // datos de los productos
+
+let productos = [];
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    productos = data.products;
+    mostrarHTML(productos);
+  })
+  .catch(error => console.error("error fetchig data:", error));
+
+
+searchInput.addEventListener('input', function () {
+  const searchText = searchInput.value.toLowerCase();
+  const filteredProductos = productos.filter(item =>
+    item.name.toLowerCase().includes(searchText) ||
+    item.description.toLowerCase().includes(searchText)
+  );
+
+  mostrarHTML(filteredProductos);
+});
+
+// funcion de mostrar los datos similar a la de showdata()
+function mostrarHTML(dataArray) {
+  contenidoProductos.innerHTML = '';
+  for (const item of dataArray) {
+    contenidoProductos.innerHTML += `
+      <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3"> <img src="${item.image}" alt="product image" class="img-thumbnail"/> </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <div class="mb-1">
+                      <h4>${item.name} - ${item.currency} ${item.cost}</h4> 
+                      <p>${item.description}</p> 
+                      </div>
+                      <small class="text-muted"> ${item.soldCount} vendidos</small> 
+                  </div>
+              </div>
+          </div>
+      </div>
+      `; 
+  }
+}
