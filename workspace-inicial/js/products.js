5818aed83a6d3b1bb24eb92b5d8ca8ef51639c4e
sebaas798 <ssoba93@gmail.com>
2023-08-31 12:44:01 -0300

    Mensaje: No hay resultados

--- workspace-inicial/js/products.js
+++ workspace-inicial/js/products.js
@@ -56,8 +56,29 @@ function filtrarPrecio(elements) {
           contFiltrado.push(element);
       }
   }
-  showData(contFiltrado);
+  if(contFiltrado.length ) {
+    showData(contFiltrado);
+  } else {
+    noResultado();
+  }
 };
+
+function noResultado() {
+
+  LimpiarHTML()
+
+  const noResultado = document.createElement("DIV");
+  noResultado.classList.add("alerta", "error");
+  noResultado.textContent = "No hay resultados"
+  contenidoProductos.appendChild(noResultado);
+}
+
+function LimpiarHTML (){
+  while(contenidoProductos.firstChild) {
+      contenidoProductos.removeChild(contenidoProductos.firstChild);
+  }
+
+}
 
 function limpiar() {
   getData(); // Vuelve a obtener los datos originales del listado
@@ -78,10 +99,12 @@ document.addEventListener("DOMContentLoaded", () => {
   );
   showData(filteredProductos);
   });
+
   //evento al hacer click en filtrar
   btnFiltrar.addEventListener("click", () => {
     filtrarPrecio(originalData);
   });
+
   //evento al hacer click en limpiar
   btnLimpiar.addEventListener("click", () => {
     limpiar();
