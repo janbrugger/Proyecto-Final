95b70dd82845ed7c9788cb127cdcac834942ec0d
Sebastian Soba <ssoba93@gmail.com>
2023-09-27 13:59:11 -0300

    iconos

--- workspace-inicial/js/init.js
+++ workspace-inicial/js/init.js
@@ -45,6 +45,7 @@ let getJSONData = function (url) {
 
 let showUserNavbar = function () { //funcion para mostrar usuario en navbar.
   if (!verificacionLogin()) {
+    // Muestra el menú desplegable desde el email del usuario.
     navbar.innerHTML += `<li class="nav-item">
    
     <div class="container-fluid">
@@ -59,9 +60,25 @@ let showUserNavbar = function () { //funcion para mostrar usuario en navbar.
           ${User.email}
           </a>
           <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Mi carrito</a></li>
-            <li><a class="dropdown-item" href="#">Mi perfil</a></li>
-            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+            <li><a class="dropdown-item" href="#">Mi carrito <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-copy" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+            <path d="M11.5 17h-5.5v-14h-2" />
+            <path d="M6 5l14 1l-1 7h-13" />
+            <path d="M15 19l2 2l4 -4" />
+          </svg></a></li>
+            <li><a class="dropdown-item" href="#">Mi perfil <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+            <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+          </svg></a></li>
+            <li><a class="dropdown-item" href="#">Cerrar sesión <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M9 12h12l-3 -3" />
+            <path d="M18 15l3 -3" />
+          </svg></a></li>
             </li>`
   }
 };
