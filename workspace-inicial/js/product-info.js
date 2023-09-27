5834c33ff3d7f7124fcbc21a2bb511716abde47a
Valentina Alonso <alonsovalentina55@gmail.com>
2023-09-27 17:56:56 -0300

    Muestra de productos relacionados

--- workspace-inicial/js/product-info.js
+++ workspace-inicial/js/product-info.js
@@ -1,18 +1,50 @@
 const productID = localStorage.getItem("productID");
 const container = document.getElementById("products-container");
-const comments = document.getElementById("comments")
+const comments = document.getElementById("comments-container")
 const rating = document.getElementById("rating");
 const selectedRating = document.getElementById("selected-rating");
 const btnComment = document.getElementById("btnComment");
 
+const relatedProducts = document.getElementById("related-products-container")
+
+//Función que trae los detalles de cada producto
 function getData(){
   try {
     fetch(PRODUCT_INFO_URL + productID + ".json")
     .then(response => response.json())
         .then(data => { 
           showProducts(data);
+          if (data.relatedProducts) {
+            getRelatedProducts(data.relatedProducts); //Trae los datos de productos relacionado en caso de haber
+          }
         })
-  } catch (error) {console.error("error fetchig data:", error)}   
+        .catch((error) => {
+          console.error("Error fetching product data:", error);
+        });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+//Función que muestra los detalles de cada producto
+function showProducts(data) {
+  container.innerHTML += `<div class="container">
+  <h1 class="p-5">${data.name}</h1>
+  <hr>
+  <h3>Precio</h3>
+  <p class="pb-4">${data.cost} ${data.currency}</p>
+  <h3>Descripción</h3>
+  <p class="pb-4">${data.description}</p>
+  <h3>Categoría</h3>
+  <p class="pb-4">${data.category}</p>
+  <h3>Cantidad vendidos</h3>
+  <p class="pb-4">${data.soldCount}</p>
+  <h3>Imagenes ilustrativas</h3>
+  <div class="d-flex flex-row">
+  
+  ${createCarrousel(data.images)}
+   </div>
+  </div>`
 };
 
 //Función que trae los comentarios ya ingresados de cada producto
@@ -26,30 +58,9 @@ function getComments(){
   } catch (error) {console.error("error fetchig data:", error)}
 };
 
-//Función que muestra los detalles de cada producto
-function showProducts(data) {
-    container.innerHTML += `<div class="container">
-    <h1 class="p-5">${data.name}</h1>
-    <hr>
-    <h3>Precio</h3>
-    <p class="pb-4">${data.cost} ${data.currency}</p>
-    <h3>Descripción</h3>
-    <p class="pb-4">${data.description}</p>
-    <h3>Categoría</h3>
-    <p class="pb-4">${data.category}</p>
-    <h3>Cantidad vendidos</h3>
-    <p class="pb-4">${data.soldCount}</p>
-    <h3>Imagenes ilustrativas</h3>
-    <div class="d-flex flex-row">
-    
-    ${createCarrousel(data.images)}
-     </div>
-    </div>`
-};
-
 //Función que muestra los comentarios ya ingresados de cada producto
 function showComments(data_comments){ 
-  container.innerHTML += '<h3>Comentarios</h3>';
+  container.innerHTML += '<h3 class="mt-4">Comentarios</h3>';
   if (data_comments.length === 0) {
       comments.innerHTML = `<h5 class="text-center text-muted" >
       No se han agregado comentarios sobre este producto</h5>`;
@@ -70,6 +81,38 @@ function showComments(data_comments){
 }
 };
 
+//Función que trae los productos relacionados
+function getRelatedProducts() {
+  try {
+    fetch(PRODUCT_INFO_URL + productID + ".json")
+      .then((response) => response.json())
+      .then((data_relatedProducts) => {
+        showRelatedProducts(data_relatedProducts);
+      });
+  } catch (error) {
+    console.error("error fetching data:", error);
+  }
+};
+
+//Función que muestra los productos relacionados
+function showRelatedProducts(data_relatedProducts) {
+  relatedProducts.innerHTML += '<h3 class="mt-4">Productos relacionados</h3>';
+  if (data_relatedProducts.length === 0) {
+    relatedProducts.innerHTML += `<h5 class="text-center text-muted">
+      No hay productos relacionados</h5>`;
+  } else {
+    for (const product of data_relatedProducts.relatedProducts) {
+      relatedProducts.innerHTML += `
+        <div class="list-group-item d-inline-block mr-2 mb-2"> 
+        <div>
+            <img src="${product.image}" class="img-fluid mt-2" style="max-width: 300px; max-height: 100px;">
+        </div>  
+        <h4 class="h6 text-center mt-2">${product.name}</h4> 
+        </div>`;
+    }
+  }
+};
+
 //Función para otorgar puntaje a través de estrellas
 function stars(quantity) {
     return "<i class='fa fa-star checked'></i>".repeat(Math.floor(quantity)) + "<i class='fa fa-star'></i>".repeat(5 - Math.floor(quantity));
@@ -81,7 +124,7 @@ function createCarrousel(images) {
     <div class="carousel-inner">
     ${images.map((image,index) => {
       return `<div class="carousel-item ${index===0 ? "active" : ""}">
-      <img src="${image}" class="d-block w-100 rounded" alt="...">
+      <img src="${image}" class="d-block rounded" alt="...">
     </div>`
     })}
     </div>
@@ -154,6 +197,6 @@ rating.addEventListener("click", (event) => {
 
   document.addEventListener("DOMContentLoaded", function() {
     showUserNavbar();
-    getData()
-    getComments()
+    getData();
+    getComments();
   });
