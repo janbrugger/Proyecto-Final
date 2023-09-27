17b160c5dae14e05b3724bd115564545c034b2f7
Jan B <janbrugger00@gmail.com>
2023-09-27 15:02:34 -0300

    dark mode
    funcional pero incompleto, falta agregarlo al menú desplegable

--- workspace-inicial/js/index.js
+++ workspace-inicial/js/index.js
@@ -15,3 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     showUserNavbar();
 });
+
+
+
+  const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  }
+
+  const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+  }
+
+  const cambiarTema = () => {
+    document.querySelector("body").getAttribute("data-bs-theme") === "light"?
+    temaOscuro() : temaClaro();
+  }
