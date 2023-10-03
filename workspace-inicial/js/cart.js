ef365b1821cc28a69f38efadf48d58ada3881c28
juanmadelgado <juanmadel2002@gmail.com>
2023-10-02 21:39:00 -0300

    fetchCarrito detalles

--- workspace-inicial/js/cart.js
+++ workspace-inicial/js/cart.js
@@ -1,25 +1,23 @@
 const userID = 25801;
 const container = document.getElementById("items");
 
-
+//Fecth Carrito
 function getCartInfo(data) {
-    return new Promise((resolve, reject) => { //la funcion devuelve una promesa
+    return new Promise((resolve, reject) => {
       fetch(data)
         .then(response => response.json())
-        .then(data => resolve(data)) //si obtenemos la data devolvemos una promesa resuelta
-        .catch(error => reject(error)) //en caso contrario devolvemos una promesa rechazada
+        .then(data => resolve(data)) 
+        .catch(error => reject(error)) 
     });
   }
-
   async function showData() {
     try {
-      let cart = await getCartInfo(CART_INFO_URL + userID + ".json"); //espera a recibir los resultados de la funcion.
+      let cart = await getCartInfo(CART_INFO_URL + userID + ".json"); 
       showCartInfo(cart);
     } catch (error) { console.log(error) }
 }
 
-//<img onclick="setProductID(${article.id})" src="${article.image}" class="img-fluid mt-2 cursor-active" style="max-width: 300px; max-height: 100px;"></img>
-
+//Mostrar info del carrito
 function showCartInfo(data_cart){
     for (const article of data_cart.articles) {
         container.innerHTML += `
