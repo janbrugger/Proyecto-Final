9c1365fba574f0b43b7b846761faa9a28d99a492
Jan B <janbrugger00@gmail.com>
2023-10-10 22:46:53 -0300

    avance 10-10 late

--- workspace-inicial/js/categories.js
+++ workspace-inicial/js/categories.js
@@ -50,12 +50,12 @@ function showCategoriesList() {
             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))) {
 
             htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
+            <div onclick="setCatID(${category.id})" class="col-12 list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
                         <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
                     </div>
-                    <div class="col">
+                    <div class="col-8">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">${category.name}</h4>
                             <small class="text-muted fs-9 fw-light">${category.productCount} artículos</small>
