f17f0c457486ee5e641e8145815c3110967eecd5
Valentina Alonso <alonsovalentina55@gmail.com>
2023-10-11 13:47:32 -0300

    Subtotal automatico

--- workspace-inicial/js/cart.js
+++ workspace-inicial/js/cart.js
@@ -1,40 +1,60 @@
 const userID = 25801;
 const container = document.getElementById("items");
+let data_cart; // Variable global para almacenar los datos del carrito
 
-//Fecth Carrito
+// Fetch Carrito
 function getCartInfo(data) {
-    return new Promise((resolve, reject) => {
-      fetch(data)
-        .then(response => response.json())
-        .then(data => resolve(data)) 
-        .catch(error => reject(error)) 
-    });
+  return new Promise((resolve, reject) => {
+    fetch(data)
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(error => reject(error))
+  });
+}
+
+// Mostrar información del carrito
+async function showData() {
+  try {
+    data_cart = await getCartInfo(CART_INFO_URL + userID + ".json");
+    showCartInfo(data_cart);
+  } catch (error) {
+    console.log(error);
   }
-  async function showData() {
-    try {
-      let cart = await getCartInfo(CART_INFO_URL + userID + ".json"); 
-      showCartInfo(cart);
-    } catch (error) { console.log(error) }
 }
 
-//Mostrar info del carrito
-function showCartInfo(data_cart){
-    for (const article of data_cart.articles) {
-        container.innerHTML += `
-              <tr>
-                <td><img onclick="setProductID(${article.id})" src="${article.image}" class="img-fluid mt-2 cursor-active" style="max-height: 80px;"></img></td>
-                <td>${article.name}</td>
-                <td>${article.currency} <span>${article.unitCost}</span></td>
-                <td><input class="col-lg-2"  value=${article.count}></td>
-                <td><strong>${article.currency} <span>${article.unitCost*article.count}</span><strong></td>
-              </tr>
-              `
-    }
+// Agregar evento input a los elementos de cantidad
+container.addEventListener("input", function (event) {
+  if (event.target.classList.contains("quantity-input")) {
+    updateSubtotal(event.target);
+  }
+});
+
+// Mostrar información del carrito
+function showCartInfo(data_cart) {
+  for (const [index, article] of data_cart.articles.entries()) {
+    container.innerHTML += `
+      <tr data-index="${index}">
+        <td><img onclick="setProductID(${article.id})" src="${article.image}" class="img-fluid mt-2 cursor-active" style="max-height: 80px;"></td>
+        <td>${article.name}</td>
+        <td>${article.currency} <span>${article.unitCost}</span></td>
+        <td><input class="col-lg-2 quantity-input" type="number" min="1" value="${article.count}"></td>
+        <td><strong>${article.currency} <span>${article.unitCost * article.count}</span></strong></td>
+      </tr>
+    `;
+  }
 }
 
+// Actualizar subtotal en tiempo real
+function updateSubtotal(inputElement) {
+  const rowIndex = inputElement.closest("tr").getAttribute("data-index");
+  const quantity = parseInt(inputElement.value);
+  const article = data_cart.articles[rowIndex];
+  const subtotal = quantity * article.unitCost;
 
+  const subtotalElement = inputElement.closest("tr").querySelector("td:last-child strong span");
+  subtotalElement.textContent = subtotal;
+}
 
-document.addEventListener("DOMContentLoaded", function (){
-showData();
-
+document.addEventListener("DOMContentLoaded", function () {
+  showData();
 });
