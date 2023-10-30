a55f6747766ff3ff0440944752ce790522ab3807
Francisco <141170084+franciscoclaus@users.noreply.github.com>
2023-10-30 19:10:03 -0300

    fix profile undefined

--- workspace-inicial/js/my-profile.js
+++ workspace-inicial/js/my-profile.js
@@ -12,31 +12,31 @@ document.addEventListener("DOMContentLoaded", function () {
     autocompleteProfileInfo()
 });
 
-document.getElementById("form").addEventListener("submit", (e)=> {
+document.getElementById("form").addEventListener("submit", (e) => {
     e.preventDefault()
     setProfileInfo()
 });
 
 
 //guarda los datos ingresados en el localStorage
-function setProfileInfo(){
+function setProfileInfo() {
 
-   User.name = nombre.value;
-   User.name2 = nombre2.value;
-   User.lastName = apellido.value;
-   User.lastName2 = apellido2.value;
-   User.email = email.value
-   User.contact = contacto.value;
-   localStorage.setItem("user", JSON.stringify(User));
+    User.name = nombre.value;
+    User.name2 = nombre2.value;
+    User.lastName = apellido.value;
+    User.lastName2 = apellido2.value;
+    User.email = email.value
+    User.contact = contacto.value;
+    localStorage.setItem("user", JSON.stringify(User));
 };
 
 //rellena los campos con la informacion del perfil al cargar la pagina.
-function autocompleteProfileInfo(){
+function autocompleteProfileInfo() {
 
-nombre.value = User.name;
-nombre2.value = User.name2;
-apellido.value = User.lastName;
-apellido2.value = User.lastName2;
-email.value = User.email;
-contacto.value = User.contact;
+    if (User.name) { nombre.value = User.name; };
+    if (User.name2) { nombre2.value = User.name2; };
+    if (User.lastName) { apellido.value = User.lastName; };
+    if (User.lastName2) { apellido2.value = User.lastName2; };
+    if (User.email) { email.value = User.email; };
+    if (User.contact) { contacto.value = User.contact; };
 }
