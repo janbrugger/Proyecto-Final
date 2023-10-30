7db8fc768209ca150451d6547cca3b8804a140b4
Francisco <141170084+franciscoclaus@users.noreply.github.com>
2023-10-30 18:36:57 -0300

    localStorage
    se guardan los datos en localStorage

--- workspace-inicial/js/my-profile.js
+++ workspace-inicial/js/my-profile.js
@@ -1,3 +1,11 @@
+const nombre = document.getElementById("nombre");
+const nombre2 = document.getElementById("nombre2");
+const apellido = document.getElementById("apellido");
+const apellido2 = document.getElementById("apellido2");
+const email = document.getElementById("email");
+const contacto = document.getElementById("contacto");
+
+
 document.addEventListener("DOMContentLoaded", function () {
     userMenu();
     themeMenu();
@@ -10,25 +18,25 @@ document.getElementById("form").addEventListener("submit", (e)=> {
 });
 
 
+//guarda los datos ingresados en el localStorage
 function setProfileInfo(){
-   const nombre = document.getElementById("nombre").value
-   const nombre2 = document.getElementById("nombre2").value
-   const apellido = document.getElementById("apellido").value
-   const apellido2 = document.getElementById("apellido2").value
-   const email = document.getElementById("email").value //se tiene que poder cambiar el mail navbar y localstorage
-   const contacto = document.getElementById("contacto").value
 
-
-   User.name = nombre;
-   User.name2 = nombre2;
-   User.lastName = apellido;
-   User.lastName2 = apellido2;
-   User.email = email
-   User.contact = contacto;
-   localStorage.setItem("user", JSON.stringify(User)); //guardamos en el storage
+   User.name = nombre.value;
+   User.name2 = nombre2.value;
+   User.lastName = apellido.value;
+   User.lastName2 = apellido2.value;
+   User.email = email.value
+   User.contact = contacto.value;
+   localStorage.setItem("user", JSON.stringify(User));
 };
 
 //rellena los campos con la informacion del perfil al cargar la pagina.
 function autocompleteProfileInfo(){
-const email = document.getElementById("email").value = User.email
+
+nombre.value = User.name;
+nombre2.value = User.name2;
+apellido.value = User.lastName;
+apellido2.value = User.lastName2;
+email.value = User.email;
+contacto.value = User.contact;
 }
