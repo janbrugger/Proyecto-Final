f75b1606333ae8359862d23841ff4e8a2c313e92
Sebastian Soba <ssoba93@gmail.com>
2023-11-06 16:16:49 -0300

    Update my-profile.js
    constantes

---
+++ workspace-inicial/js/my-profile.js
@@ -0,0 +1,6 @@
+const primerNombre = document.querySelector("#nombre");
+const segundoNombre = document.querySelector("#nombre2");
+const primerApellido = document.querySelector("#apellido");
+const segundoApellido = document.querySelector("#apellido2");
+const email = document.querySelector("#email");
+const telefono = document.querySelector("#contacto");
