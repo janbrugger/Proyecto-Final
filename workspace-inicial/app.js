73362cbe6971770368887e4a68e2dc22c48988b9
juanmadelgado <juanmadel2002@gmail.com>
2023-11-26 15:11:48 -0300

    crud backend al carrito

--- workspace-inicial/app.js
+++ workspace-inicial/app.js
@@ -1,3 +1,4 @@
+//app.js
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = "CLAVE SECRETA"
@@ -7,6 +8,14 @@ const categoryProductRouter = require("./routes/categoryProductRouter");
 const productsRouter = require("./routes/productsRouter");
 const commentsRouter = require("./routes/commentsRouter");
 const buyRouter = require("./routes/buyRouter");
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+  host: "localhost", 
+  user: "root", 
+  password: "trece00R", 
+  database: "carrito",
+  connectionLimit: 10});
 
 const app = express();
 const port = 3000;
@@ -14,6 +23,96 @@ const port = 3000;
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// GET a carrito
+app.get("/articles", async(req, res) => {
+  let conn;
+  try {
+
+	conn = await pool.getConnection();
+	const rows = await conn.query(
+    "SELECT id, name, quantity, cost, currency, image FROM articles");
+
+      res.json(rows);
+  }catch{
+    res.status(500).json({message:"Se rompió el servidor"})
+  }
+
+   finally {
+	if (conn) conn.release(); //release to pool
+  }
+});
+
+// POST a carrito
+app.post("/articles", async (req, res) => {
+  /* La propiedad "body" del request permite acceder a los datos 
+       que se encuentran en el cuerpo de la petición */
+
+       let conn;
+       try {
+     
+       conn = await pool.getConnection();
+       const response = await conn.query(
+        `INSERT INTO articles(name, quantity, cost, currency, image) VALUE(?, ?, ?, ?, ?)`,
+        [req.body.name, req.body.quantity, req.body.cost, req.body.currency, req.body.image]
+        );
+     
+           res.json({id: parseInt(response.insertId), ...req.body});
+       }catch{
+         res.status(500).json({message:"Se rompió el servidor"})
+       }
+     
+        finally {
+       if (conn) conn.release(); //release to pool
+       }
+});
+
+// PUT a carrito
+app.put("/articles/:id", async (req, res) => {
+  /* COMPLETA EL CÓDIGO NECESARIO:
+     Para que se pueda actualizar el objeto asociado al índice indicado en la URL 
+   */
+     let conn;
+     try {
+   
+     conn = await pool.getConnection();
+     const response = await conn.query(
+      `UPDATE articles SET name=?, quantity=?, cost=?, currency=?, image=? WHERE id=?`,
+       [req.body.name, req.body.quantity, req.body.cost, req.body.currency, req.body.image, req.params.id]
+      );
+   
+         res.json({id: parseInt(response.insertId), ...req.body});
+     }catch{
+       res.status(500).json({message:"Se rompió el servidor"})
+     }
+   
+      finally {
+     if (conn) conn.release(); //release to pool
+     }
+});
+
+// DELETE a carrito
+app.delete("/articles/:id", async(req, res) => {
+  /* COMPLETA EL CÓDIGO NECESARIO:
+     Para que se pueda eliminar el objeto asociado al índice indicado en la URL 
+   */
+     let conn;
+     try {
+   
+     conn = await pool.getConnection();
+     const response = await conn.query(
+       `DELETE FROM articles WHERE id=?;`, req.params.id);
+   
+         res.json({index: parseInt(response.insertId), ...req.body});
+     }catch{
+       res.status(500).json({message:"Se rompió el servidor"})
+     }
+   
+      finally {
+     if (conn) conn.release(); //release to pool
+     }
+    });
+
 
 // Auth
 app.post("/login", (req,res)=>{
@@ -76,6 +175,7 @@ app.use("/api/cart", (req, res, next) => {
   }
 });
 //----
+
 
 
 app.use("/api/users", userRouter); //users
